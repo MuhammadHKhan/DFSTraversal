670239fa06fb5609d35dae7be97258458a111fd4
Muhammad Khan <64335488+MuhammadHKhan@users.noreply.github.com>
2022-04-01 12:32:12 -0700

    Update DFSTraversalStreets.cpp

--- DFSTraversalStreets.cpp
+++ DFSTraversalStreets.cpp
@@ -4,16 +4,6 @@
 #include "vertex.h"
 #include "stack.h"
 
-/*Name: Muhammad Khan, Assignment 9 CS 302 1001
-  This program is going to use a DFS traversal to find 
-  all the paths that are "Two ways" in the linked list.
-  The DFS traversal boolean function will go through all of
-  the nodes current neigbors to find if a two way path exists, 
-  and if it does, it will push it on to the stack we have created
-  by using the stack class. We will also be using an iterator to 
-  iterate through the function to find if any two way paths exist, 
-  and if they do, we will print them to the screen.
-  *///
 
 using namespace std;
 
